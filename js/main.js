7abe440f8047382d654fa70fa5a32b6e82d5a578
Bobo <294835908@qq.com>
2017-04-05 10:58:32 +0800

    哈哈

--- js/main.js
+++ js/main.js
@@ -3,7 +3,32 @@ define([],function() {
     //配置
     require.config({
 
-        baseUrl: './'
+        baseUrl: './',
+
+        paths: {
+            advertAdd: 'js/advert/advert_add',
+            advertList: 'js/advert/advert_List',
+            courseAdd1: 'js/course/course_add_step1',
+            courseAdd2: 'js/course/course_add_step2',
+            courseAdd3: 'js/course/course_add_step3',
+            courseAdd: 'js/course/course_add',
+            courseCategoryAdd: 'js/course/course_category_add',
+            courseCategory: 'js/course/course_category',
+            courseList: 'js/course/course_list',
+            courseTopic: 'js/course/course_topic',
+            login: 'js/home/login',
+            repass: 'js/home/repass',
+            settings: 'js/home/settings',
+            teacherAdd: 'js/teacher/teacher_add',
+            teacherList: 'js/teacher/teacher_list',
+            userProfile: 'js/user/user_profile',
+            userList: 'js/user/user_list',
+
+            //配置第三方js模块别名
+            template:'lib/artTemplate/template-debug',
+            bootstrap: 'lib/bootstrap/js/bootstrap',
+            bootstrap: 'lib/bootstrap/js/bootstrap',
+        },
     });
 
 
